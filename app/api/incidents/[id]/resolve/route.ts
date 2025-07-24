4f357d4f04f87ecf9b762648f9ed68b461b3e301
Suraj Chakraborty <76647754+suraj-chakraborty@users.noreply.github.com>
2025-07-24 11:52:41 +0530

    build fix

--- app/api/incidents/[id]/resolve/route.ts
+++ app/api/incidents/[id]/resolve/route.ts
@@ -1,7 +1,8 @@
 import { prisma } from "@/app/lib/PrismaClient";
 import { NextResponse } from "next/server";
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function PUT(req: Request, { params }: any) {
     const updated = await prisma.incident.update({
         where: { id: params.id },
         data: { resolved: true },
